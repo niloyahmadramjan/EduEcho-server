be7fb67da75fa02da1336cc35fd6d296de9cf3cd
niloyahmadramjan <niloyahmadramjan@gmail.com>
2025-06-18 01:51:22 +0800

    compalte assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const jwt = require("jsonwebtoken"); // ✅ Added JWT
+const jwt = require("jsonwebtoken"); 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
